55835e6d63bb6409dbcf379b6713d8fe75e8d10d
Paul Fornage <paul@fornage.net>
2023-08-25 22:44:16 -0700

    finished tracing problems

--- scripts/content.js
+++ scripts/content.js
@@ -33,11 +33,17 @@ setInterval(function() {
     console.log("loading")
     loading_stage = (document.getElementsByClassName("zybook-section-title").length === 0);
   } else {
-    if(!listeners_attached){
-      listeners_attached = attach_button_event_listeners();
-    }
+    attach_button_event_listeners();
   }
-}, 100);
+  remove_watermarks();
+}, 200);
+
+async function remove_watermarks(){
+  const watermark_DOMs = document.getElementsByClassName("activity-watermark");
+  for(let i = 0; i < watermark_DOMs.length; i ++){
+    watermark_DOMs.item(i).textContent = "foiled again";
+  }
+}
 
 function attach_button_event_listeners(){
   let current_div_ids = [];
@@ -55,12 +61,15 @@ function attach_button_event_listeners(){
       for (let k = 0; k < button_divs.length; k++) {
 
         button_div_id = ("FZYB_" + (problem_solvers[i].problem_type_string) + "_" + j);
-        button_divs.item(k).id = button_div_id;
+        if(!(button_divs.item(k).id === button_div_id)){
+          button_divs.item(k).id = button_div_id;
+
+          const copy_of_current_div_id = current_div_id; // probably needed to avoid events calling function with reference of new variable
+          document.getElementById(button_div_id).addEventListener('mouseup', (event) => {
+            problem_solvers[i].click_event_handler(event, copy_of_current_div_id);
+          });
+        }
         attached = true;
-        const copy_of_current_div_id = current_div_id; // probably needed to avoid events calling function with reference of new variable
-        document.getElementById(button_div_id).addEventListener('mouseup', (event) => {
-          problem_solvers[i].click_event_handler(event, copy_of_current_div_id);
-        });
       }
     }
   }
@@ -72,29 +81,95 @@ function get_drag_and_drop_div_ids(){
 }
 
 async function enter_output_div_click_event_handler(event, enter_output_div_id){
-  set_title(enter_output_div_id, "Enter output problems not yet implemented");
-// #include <iostream>
-// using namespace std;
-//
-// int main() {
-//   int numItems;
-//
-//   numItems = 5;
-//
-//   if (numItems > 3) {
-//     cout << "c" << endl;
-//   }
-//   else if (numItems < 8) {
-//     cout << "e" << endl;
-//   }
-//   else {
-//     cout << "k" << endl;
-//   }
-//
-//   cout << "r" << endl;
-//
-//   return 0;
-// }
+  let experienced_error = false;
+  let code_output = "";
+
+  const continue_loop = function (root_section_div_id){
+    const done_DOMs = document.getElementById(root_section_div_id).getElementsByClassName("zyante-progression-is-done correct");
+    return !((done_DOMs.length === 1) && done_DOMs.item(0).style.display === "");
+
+  }
+
+  const check_correctness = async function (root_section_div_id) {
+    let timed_out = false;
+
+    const timeoutID = setTimeout(() => {
+      timed_out = true;
+    }, 1000);
+
+    let check_DOM = document.getElementById(root_section_div_id).getElementsByClassName("zyante-progression-checkmark");
+    let x_mark_DOM = document.getElementById(root_section_div_id).getElementsByClassName("zyante-progression-x-mark");
+
+    let correct = ((check_DOM.length === 1) && check_DOM.item(0).style.display === "")
+    let incorrect = ((x_mark_DOM.length === 1) && x_mark_DOM.item(0).style.display === "")
+    while (!(correct || incorrect || timed_out)) {
+      check_DOM = document.getElementById(root_section_div_id).getElementsByClassName("zyante-progression-checkmark");
+      x_mark_DOM = document.getElementById(root_section_div_id).getElementsByClassName("zyante-progression-x-mark");
+      correct = ((check_DOM.length === 1) && check_DOM.item(0).style.display === "")
+      incorrect = ((x_mark_DOM.length === 1) && x_mark_DOM.item(0).style.display === "")
+      await sleep(100);
+    }
+    clearTimeout(timeoutID);
+    return !incorrect;
+
+  }
+
+  set_title(enter_output_div_id, "Working...");
+  let start_button_DOMs = document.getElementById(enter_output_div_id).getElementsByClassName("zyante-progression-start-button button");
+  if(start_button_DOMs.length > 0){
+    start_button_DOMs.item(0).click()
+  }
+  await sleep(200);
+
+  while(continue_loop(enter_output_div_id) && !experienced_error){ // TODO: make this not an ugly ass nesting of various error checks.
+    await sleep(200);
+    let code_DOMs = document.getElementById(enter_output_div_id).getElementsByClassName("code");
+    let code_plain_text = "";
+    if(!(code_DOMs.length === 1)) {
+      set_title(enter_output_div_id, "Error: no code found");
+      experienced_error = true;
+    } else {
+      code_plain_text = code_DOMs.item(0).innerText;
+      code_output = await compile_cpp(code_plain_text)
+      if(code_output === undefined){
+        experienced_error = true;
+        set_title(enter_output_div_id, "Error: API did not produce an output");
+      } else {
+        let input_DOMs = document.getElementById(enter_output_div_id).getElementsByClassName("console");
+        if(!(input_DOMs.length === 1)) {
+          set_title(enter_output_div_id, "Error: no text area found");
+          experienced_error = true;
+        } else {
+          input_DOMs.item(0).value = code_output.replace("\\n", "\n");
+          input_DOMs.item(0).dispatchEvent(e);
+          const check_output_DOMs = document.getElementById(enter_output_div_id).getElementsByClassName("zyante-progression-check-button button");
+          if(!(check_output_DOMs.length === 1)) {
+            set_title(enter_output_div_id, "Error: No \'check\' button found");
+            experienced_error = true;
+          } else {
+            check_output_DOMs.item(0).click();
+            if(!await check_correctness(enter_output_div_id))
+            {
+              set_title(enter_output_div_id, "Error: Incorrect answer or timed out waiting for feedback");
+              experienced_error = true;
+            } else {
+              await sleep(100);
+              const next_DOMs = document.getElementById(enter_output_div_id).getElementsByClassName("zyante-progression-next-button button");
+              if(!(next_DOMs.length === 1)) {
+                set_title(enter_output_div_id, "Error: No \'next\' button found");
+                experienced_error = true;
+              } else {
+                next_DOMs.item(0).click()
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  if(!continue_loop(enter_output_div_id)){
+    set_title(enter_output_div_id, "Done");
+  }
 }
 
 async function drag_and_drop_div_click_event_handler(event, drag_and_drop_div_id){
@@ -102,7 +177,18 @@ async function drag_and_drop_div_click_event_handler(event, drag_and_drop_div_id
 }
 
 function get_enter_output_div_ids(){
-  return(get_DOM_ids_from_class_name("interactive-activity-container custom-content-resource challenge large ember-view")); // TODO: this class name is not specific to enter output questions
+  let real_enter_output_div_ids = [];
+  const potential_enter_output_div_ids = get_DOM_ids_from_class_name("interactive-activity-container custom-content-resource challenge large ember-view");
+  const num_potential_enter_output_div_ids = potential_enter_output_div_ids.length
+  if(num_potential_enter_output_div_ids === 0){
+    return real_enter_output_div_ids;
+  }
+  for(let i = 0; i < num_potential_enter_output_div_ids; i++){
+    if((document.getElementById(potential_enter_output_div_ids[i]).getElementsByClassName("custom-tool codeOutput progressionTool").length) === 1){
+      real_enter_output_div_ids.push(potential_enter_output_div_ids[i]);
+    }
+  }
+  return real_enter_output_div_ids;
 }
 
 function get_animation_div_ids(){
@@ -183,7 +269,7 @@ async function short_answer_div_click_event_handler(event, short_answer_div_id){
   }
 }
 
-async function solve_individual_short_answer_problem_by_id(short_answer_problem_id){ // TODO: this function does not work
+async function solve_individual_short_answer_problem_by_id(short_answer_problem_id){
   let individual_short_answer = document.getElementById(short_answer_problem_id);
 
   while(individual_short_answer.getElementsByClassName("zb-explanation has-explanation correct").length === 0)
@@ -309,4 +395,36 @@ function set_title(div_id, new_title){
   }
 }
 
-
+async function compile_cpp(code_string) {
+  let timed_out = false;
+  const timeoutID = setTimeout(() => {
+    timed_out = true;
+  }, 10000);
+  let response_output = ""; // this value is used if the API request times out
+  let response_received = false;
+  const data = JSON.stringify({
+    language: 'cpp17',
+    version: 'latest',
+    code: code_string,
+    input: null
+  });
+  const xhr = new XMLHttpRequest();
+  xhr.withCredentials = true;
+  xhr.addEventListener('readystatechange', function () {
+    if (this.readyState === this.DONE) {
+      const response_json = JSON.parse(this.responseText);
+      response_output = response_json.output;
+      response_received = true;
+    }
+  });
+  xhr.open('POST', 'https://online-code-compiler.p.rapidapi.com/v1/');
+  xhr.setRequestHeader('content-type', 'application/json');
+  xhr.setRequestHeader('X-RapidAPI-Key', '591f261ff4msh26f3f44db0aa77bp1c5c8fjsn002736b5e301');
+  xhr.setRequestHeader('X-RapidAPI-Host', 'online-code-compiler.p.rapidapi.com');
+  xhr.send(data);
+  while (!(response_received || timed_out)) {
+    await sleep(10);
+  }
+  clearTimeout(timeoutID);
+  return response_output;
+}
